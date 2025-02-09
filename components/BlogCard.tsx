2d3299358b9f41ee7044ce0d975bec6da7c1d696
Pedro Almeida <pedroh_7@hotmail.com>
2025-02-09 16:01:22 -0300

    feat: Rework BlogCard design

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,27 +1,35 @@
 import { cn } from "@/lib/utils";
+import { Button } from "./Button";
+import { ArrowUpRight } from "lucide-react";
 
 export const BlogCard = ({
   className,
   title,
   description,
+  index = '01'
 }: {
   className?: string;
   title: string;
   description: string;
+  index?: string;
 }) => {
   return (
-    <div className={cn(className, "border-2 border-gray-200")}>
-      <div className={"h-16 w-full"}>
-        <div className="relative h-screen">
-          <div className="absolute inset-0">
-            <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"></div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
+    <div
+      className={cn(
+        className,
+        "rounded-xl overflow-hidden p-6 bg-gradient-to-tl from-indigo-50/50 to-neutral-50"
+      )}
+    >
+      <div className="text-xl text-indigo-500/70 underline mb-4">{index}</div>
+      <h3 className="w-fit text-xl font-semibold tracking-tight mb-2">
+        {title}
+      </h3>
+
+      <p className="text-xs text-justify text-ellipsis line-clamp-2">{description}</p>
+
+      <Button className="mt-6" variant={'link'} RightIcon={<ArrowUpRight size={18} />}>
+        Ler mais
+      </Button>
     </div>
   );
 };
