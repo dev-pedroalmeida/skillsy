dd4a1fb27d97c20c92c789864c00b54298d16430
Pedro Almeida <pedroh_7@hotmail.com>
2025-02-09 15:24:26 -0300

    style: Change background pattern color

--- components/BentoGrid.tsx
+++ components/BentoGrid.tsx
@@ -28,11 +28,11 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "bg-gray-100 p-6 rounded-lg shadow-sm flex flex-col row-span-1",
+        "bg-white p-6 rounded-lg shadow-sm flex flex-col row-span-1",
         className
       )}
     >
-      <div className="flex flex-1 w-full min-h-[6rem] rounded-xl bg-gray-50/60 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
+      <div className="flex flex-1 w-full min-h-[6rem] rounded-xl bg-gray-50 bg-[radial-gradient(#818cf8_1px,transparent_1px)] [background-size:16px_16px]"></div>
       
       <header className="my-4">
         <h3 className="text-xl font-semibold">{title}</h3>
