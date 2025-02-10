a9831691aa34851a8c60f1fcd343455af90e089e
Pedro Almeida <pedroh_7@hotmail.com>
2025-02-09 23:59:56 -0300

    style: Change BlogCard background

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -6,7 +6,7 @@ export const BlogCard = ({
   className,
   title,
   description,
-  index = '01'
+  index = "01",
 }: {
   className?: string;
   title: string;
@@ -15,19 +15,22 @@ export const BlogCard = ({
 }) => {
   return (
     <div
-      className={cn(
-        className,
-        "rounded-xl overflow-hidden p-6 bg-gradient-to-tl from-indigo-50/50 to-neutral-50"
-      )}
+      className={cn(className, "rounded-xl overflow-hidden p-6 bg-neutral-100")}
     >
       <div className="text-xl text-indigo-500/70 underline mb-4">{index}</div>
       <h3 className="w-fit text-xl font-semibold tracking-tight mb-2">
         {title}
       </h3>
 
-      <p className="text-xs text-justify text-ellipsis line-clamp-2">{description}</p>
+      <p className="text-xs text-justify text-ellipsis line-clamp-2">
+        {description}
+      </p>
 
-      <Button className="mt-6" variant={'link'} RightIcon={<ArrowUpRight size={18} />}>
+      <Button
+        className="mt-6"
+        variant={"link"}
+        RightIcon={<ArrowUpRight size={18} />}
+      >
         Ler mais
       </Button>
     </div>
