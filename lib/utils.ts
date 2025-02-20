500fd02073f6f0febe339c5ebf945bba1a7dac7d
Pedro Almeida <pedroh_7@hotmail.com>
2025-02-20 14:15:24 -0300

    feat: Add shadcn sidebar

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,5 @@
-import clsx from "clsx";
-import { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
